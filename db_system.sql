abdbce16176419a9842ee5fef71997baa321b070
Adryel~ <adryel.giovaniny@gmail.com>
2020-10-20 17:46:18 -0300

    Bando de dados

---
+++ db_system.sql
@@ -0,0 +1,41 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 10.4.10-MariaDB : Database - db_system
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `tbl_foto_produto` */
+
+CREATE TABLE `tbl_foto_produto` (
+  `id_foto` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_foto` varchar(200) NOT NULL,
+  `fk_produto` int(11) NOT NULL,
+  PRIMARY KEY (`id_foto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `tbl_foto_produto` */
+
+/*Table structure for table `tbl_produto` */
+
+CREATE TABLE `tbl_produto` (
+  `id_produto` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_produto` varchar(100) NOT NULL,
+  `valor_produto` decimal(10,2) NOT NULL,
+  `modelo_produto` varchar(100) NOT NULL,
+  `marca_produto` varchar(100) NOT NULL,
+  `descricao_produto` mediumtext NOT NULL,
+  PRIMARY KEY (`id_produto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `tbl_produto` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
